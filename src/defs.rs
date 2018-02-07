8ad777e428d97bb35e554b1336f32f890703a5e1
Anton <singaraiona@gmail.com>
2018-02-07 15:19:43 +0200

    movs

--- src/defs.rs
+++ src/defs.rs
@@ -1,13 +1,22 @@
 use std::io::prelude::*;
 use std::io::Cursor;
-//
-const REX:       u8 = 0b01000000;
-const LONG_MODE: u8 = 1 << 3;
-const MODREGRM:  u8 = 0b11000000;
+use std::mem;
+
+// REX
+const REX:   u8 = 0b01001000;
+// ModRegR/M
+const MOD_ADDR_REG:       u8 = 0xc0;
+const MOD_ADDR_WO_OFFSET: u8 = 0x00;
+const MOD_ADDR_W_OFFSET:  u8 = 0x80;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Error {
+    Serialize,
+}
 //
 #[repr(u8)]
 #[derive(Debug, Clone, Copy)]
-pub enum Reg {
+pub enum Ireg {
    RAX, // 0.000
    RCX, // 0.001
    RDX, // 0.010
@@ -26,14 +35,34 @@ pub enum Reg {
    R15, // 1.111
 }
 
-impl Reg {
-    pub fn domain(&self) -> u8 { *self as u8 / 8 }
-    pub fn offset(&self) -> u8 { *self as u8 % 8 }
+impl Ireg {
+    pub fn rex(&self) -> u8 { *self as u8 / 8 }
+    pub fn reg(&self) -> u8 { *self as u8 % 8 }
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum Freg {
+    XMM0,
+    XMM1,
+    XMM2,
+    XMM3,
+    XMM4,
+    XMM5,
+    XMM6,
+    XMM7,
+}
+
+impl Freg {
+    pub fn reg(&self) -> u8 { *self as u8 }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Operand {
-    Reg(Reg),
+    Ireg(Ireg),
+    Freg(Freg),
+    Iimm(i64),
+    Fimm(f64),
     Mem,
 }
 
@@ -42,78 +71,120 @@ pub enum Instruction {
     Sub(Operand, Operand),
     Mul(Operand, Operand),
     Mov(Operand, Operand),
+    Cmp(Operand, Operand),
     Push(Operand),
     Pop(Operand),
     Ret,
 }
+//
+macro_rules! or          { ($e:expr)                                 => { $e.or_else(|_| Err(Error::Serialize))?   }}
+macro_rules! push_prefix { ($w:expr, $op1:expr, $op2:expr)           => { or!($w.write(&[REX | $op1 << 2 | $op2])) }}
+macro_rules! push_opcode { ($w:expr, $($op:expr),+)                  => { or!($w.write(&[$($op),+]))               }}
+macro_rules! push_modreg { ($w:expr, $md:expr, $op1:expr, $op2:expr) => { or!($w.write(&[$md | $op1 << 3 | $op2])) }}
+macro_rules! push_immi64 { ($w:expr, $im:expr)                       => {
+                            unsafe { or!($w.write(&mem::transmute::<i64, [u8;8]>($im))) }}}
 
 impl Instruction {
-    pub fn serialize<W: Write + Seek>(&self, writer: &mut W) {
+    pub fn serialize<W: Write + Seek>(&self, writer: &mut W) -> Result<(), Error> {
         use self::Instruction::*;
         use self::Operand::*;
         match *self {
             Ret => {
-                writer.write(&[0xc3]);
+                push_opcode!(writer, 0xc3);
             },
             Add(op1, op2) => {
                 match (op1, op2) {
-                    (Reg(r1), Reg(r2)) => {
-                        writer.write(&[REX | LONG_MODE | r2.domain() << 2 | r1.domain(),
-                                     0x01,
-                                     MODREGRM | r2.offset() << 3 | r1.offset()]);
+                    (Ireg(r1), Ireg(r2)) => {
+                        push_prefix!(writer, r2.rex(), r1.rex());
+                        push_opcode!(writer, 0x01);
+                        push_modreg!(writer, MOD_ADDR_REG, r2.reg(), r1.reg());
+                    }
+                    // ADDSD
+                    (Freg(r1), Freg(r2)) => {
+                        push_opcode!(writer, 0xf2, 0x0f, 0x58);
+                        push_modreg!(writer, MOD_ADDR_REG, r1.reg(), r2.reg());
                     }
                     _ => unimplemented!(),
                 }
             }
             Sub(op1, op2) => {
                 match (op1, op2) {
-                    (Reg(r1), Reg(r2)) => {
-                        writer.write(&[REX | LONG_MODE | r2.domain() << 2 | r1.domain(),
-                        0x29,
-                        MODREGRM | r2.offset() << 3 | r1.offset()]);
+                    (Ireg(r1), Ireg(r2)) => {
+                        push_prefix!(writer, r2.rex(), r1.rex());
+                        push_opcode!(writer, 0x29);
+                        push_modreg!(writer, MOD_ADDR_REG, r2.reg(), r1.reg());
                     }
                     _ => unimplemented!(),
                 }
             }
             Mul(op1, op2) => {
                 match (op1, op2) {
-                    (Reg(r1), Reg(r2)) => {
-                        writer.write(&[REX | LONG_MODE | r1.domain() << 2 | r2.domain(),
-                        0x0f, 0xaf,
-                        MODREGRM | r1.offset() << 3 | r2.offset()]);
+                    (Ireg(r1), Ireg(r2)) => {
+                        push_prefix!(writer, r1.rex(), r2.rex());
+                        push_opcode!(writer, 0x0f, 0xaf);
+                        push_modreg!(writer, MOD_ADDR_REG, r1.reg(), r2.reg());
                     }
                     _ => unimplemented!(),
                 }
             }
             Mov(op1, op2) => {
                 match (op1, op2) {
-                    (Reg(r1), Reg(r2)) => {
-                        writer.write(&[REX | r2.domain() << 2 | r1.domain(),
-                        0x89,
-                        MODREGRM | r2.offset() << 3 | r1.offset()]);
+                    // MOVQ
+                    (Ireg(r1), Ireg(r2)) => {
+                        push_prefix!(writer, r2.rex(), r1.rex());
+                        push_opcode!(writer, 0x89);
+                        push_modreg!(writer, MOD_ADDR_REG, r2.reg(), r1.reg());
                     }
+                    // MOVDQA
+                    (Freg(r1), Freg(r2)) => {
+                        push_opcode!(writer, 0x66, 0x0f, 0x6f);
+                        push_modreg!(writer, MOD_ADDR_REG, r1.reg(), r2.reg());
+                    }
+                    (Ireg(r1), Iimm(i2)) => {
+                        push_prefix!(writer, 0, 0);
+                        push_opcode!(writer, 0xb8);
+                        push_immi64!(writer, i2);
+                    }
+                    _ => unimplemented!(),
+                }
+            }
+            Cmp(op1, op2) => {
+                match (op1, op2) {
+                    (Ireg(r1), Ireg(r2)) => {
+                        push_prefix!(writer, r2.rex(), r1.rex());
+                        push_opcode!(writer, 0x39);
+                        push_modreg!(writer, MOD_ADDR_REG, r2.reg(), r1.reg());
+                    }
+                    //// MOVDQA
+                    //(Freg(r1), Freg(r2)) => {
+                        //push_opcode!(writer, 0x66, 0x0f, 0x6f);
+                        //push_modreg!(writer, MOD_ADDR_REG, r1.reg(), r2.reg());
+                    //}
                     _ => unimplemented!(),
                 }
             }
             Push(op1) => {
                 match op1 {
-                    Reg(r1) => {
-                        if r1.domain() == 1 { writer.write(&[REX | 1]); }
-                        writer.write(&[0x50 | r1.offset()]);
+                    Ireg(r1) => {
+                        if r1.rex() == 1 { push_prefix!(writer, 0, 1); }
+                        push_opcode!(writer, 0x50 | r1.reg());
                     }
                     _ => unimplemented!(),
                 }
             }
             Pop(op1) => {
                 match op1 {
-                    Reg(r1) => {
-                        if r1.domain() == 1 { writer.write(&[REX | 1]); }
-                        writer.write(&[0x58 | r1.offset()]);
+                    Ireg(r1) => {
+                        if r1.rex() == 1 { push_prefix!(writer, 0, 1); }
+                        push_opcode!(writer, 0x58 | r1.reg());
                     }
                     _ => unimplemented!(),
                 }
             }
+
             _ => unimplemented!(),
         }
+        Ok(())
     }
 }
+
--- tests/rasm.rs
+++ tests/rasm.rs
@@ -4,53 +4,80 @@ extern crate test;
 extern crate rasm;
 
 use rasm::x64::Assembler;
-use rasm::defs::{Instruction, Operand, Reg};
+use rasm::defs::{Instruction, Operand, Ireg, Freg};
 
-type JitFn = extern "win64" fn(i64, i64) -> i64;
+type MonadI64 = extern "win64" fn(i64)      -> i64;
+type DyadI64  = extern "win64" fn(i64, i64) -> i64;
+type DyadF64  = extern "win64" fn(f64, f64) -> f64;
 
 macro_rules! test {
-    ($fun:tt; $($ins:expr),+; $($prms:expr),+; $res:expr) => {
+    ($fun:tt; $($ins:expr),+; $jf:ty; $($prms:expr),+; $res:expr) => {
         #[test]
+        #[allow(unused)]
         fn $fun() {
+            use Instruction::*;
+            use Operand::*;
+            use Ireg::*;
+            use Freg::*;
             let mut asm = Assembler::new();
             $(asm.push_instruction($ins);)+
             asm.push_instruction(Instruction::Ret);
             let ops = asm.commit().unwrap();
             println!("{}", asm.buffer_fmt());
-            let fun = callable!(ops, JitFn);
+            let fun = callable!(ops, $jf);
             let ret = fun($($prms),+);
             assert_eq!(ret, $res);
         }
     }
 }
+
 // Tests
-test!(add_reg_reg;
-    Instruction::Mov(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RCX)),
-    Instruction::Add(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RDX));
+test!(add_reg_reg_i64;
+    Mov(Ireg(RAX), Ireg(RCX)),
+    Add(Ireg(RAX), Ireg(RDX));
+    DyadI64;
     2, 3;
     5
 );
 
+test!(add_reg_imm_i64;
+    Mov(Ireg(RAX), Iimm(9223372036854775801)),
+    Add(Ireg(RAX), Ireg(RCX));
+    MonadI64;
+    6;
+    9223372036854775807
+);
+
+test!(add_reg_reg_f64;
+    Add(Freg(XMM0), Freg(XMM1));
+    DyadF64;
+    2.1, 3.2;
+    5.300000000000001
+);
+
 test!(sub_reg_reg;
-    Instruction::Mov(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RCX)),
-    Instruction::Sub(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RDX));
+    Mov(Ireg(RAX), Ireg(RCX)),
+    Sub(Ireg(RAX), Ireg(RDX));
+    DyadI64;
     2, 3;
     -1
 );
 
 test!(mul_reg_reg;
-    Instruction::Mov(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RCX)),
-    Instruction::Mul(Operand::Reg(Reg::RAX), Operand::Reg(Reg::RDX));
+    Mov(Ireg(RAX), Ireg(RCX)),
+    Mul(Ireg(RAX), Ireg(RDX));
+    DyadI64;
     2, 3;
     6
 );
 
 test!(mul_push_pop;
-    Instruction::Push(Operand::Reg(Reg::RCX)),
-    Instruction::Push(Operand::Reg(Reg::RDX)),
-    Instruction::Pop(Operand::Reg(Reg::RAX)),
-    Instruction::Pop(Operand::Reg(Reg::R9)),
-    Instruction::Mul(Operand::Reg(Reg::RAX), Operand::Reg(Reg::R9));
+    Push(Ireg(RCX)),
+    Push(Ireg(RDX)),
+    Pop(Ireg(RAX)),
+    Pop(Ireg(R9)),
+    Mul(Ireg(RAX), Ireg(R9));
+    DyadI64;
     2, 3;
     6
 );
