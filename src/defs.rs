f171c02337937e4cbd0d3bcd7f3fe3d773288e4e
Anton <singaraiona@gmail.com>
2018-02-08 17:02:10 +0200

    conditional jumps

--- src/defs.rs
+++ src/defs.rs
@@ -62,17 +62,46 @@ pub enum Operand {
 }
 //
 pub enum Instruction {
-    Add(Operand, Operand),
-    Sub(Operand, Operand),
-    Mul(Operand, Operand),
-    Mov(Operand, Operand),
-    Cmp(Operand, Operand),
-    Jmp(Operand),
-    Jne(Operand),
-    Push(Operand),
-    Pop(Operand),
-    SetLbl(&'static str),
-    Call(Operand),
-    Ret,
+    Add(Operand, Operand), // Add r64 to r/m64/xmm.
+    Sub(Operand, Operand), // Subtract r/m64/xmm from r64.
+    Mul(Operand, Operand), // Quadword register ← r/m64∗ immediate doubleword.
+    Mov(Operand, Operand), // Move r/m64/imm to r64.
+    Cmp(Operand, Operand), // Compare r/m64with r64.
+    Jmp(Operand),          // Jump near, relative, RIP = RIP + 32-bit displacement sign extended to 64-bits
+    Ja(Operand),           // Jump near if above (CF=0 and ZF=0)
+    Jae(Operand),          // Jump near if above or equal (CF=0)
+    Jb(Operand),           // Jump near if below (CF=1)
+    Jbe(Operand),          // Jump near if below or equal (CF=1 or ZF=1)
+    Jc(Operand),           // Jump near if carry (CF=1)
+    Je(Operand),           // Jump near if equal (ZF=1)
+    Jg(Operand),           // Jump near if greater (ZF=0 and SF=OF)
+    Jge(Operand),          // Jump near if greater or equal (SF=OF)
+    Jl(Operand),           // Jump near if less (SF<>OF)
+    Jle(Operand),          // Jump near if less or equal (ZF=1 or SF<>OF)
+    Jna(Operand),          // Jump near if not above (CF=1 or ZF=1)
+    Jnae(Operand),         // Jump near if not above or equal (CF=1)
+    Jnb(Operand),          // Jump near if not below (CF=0)
+    Jnbe(Operand),         // Jump near if not below or equal (CF=0 and ZF=0)
+    Jnc(Operand),          // Jump near if not carry (CF=0)
+    Jne(Operand),          // Jump near if not equal (ZF=0)
+    Jng(Operand),          // Jump near if not greater (ZF=1 or SF<>OF)
+    Jnge(Operand),         // Jump near if not greater or equal (SF<>OF)
+    Jnl(Operand),          // Jump near if not less (SF=OF)
+    Jnle(Operand),         // Jump near if not less or equal (ZF=0 and SF=OF)
+    Jno(Operand),          // Jump near if not overflow (OF=0)
+    Jnp(Operand),          // Jump near if not parity (PF=0)
+    Jns(Operand),          // Jump near if not sign (SF=0)
+    Jnz(Operand),          // Jump near if not zero (ZF=0)
+    Jo(Operand),           // Jump near if overflow (OF=1)
+    Jp(Operand),           // Jump near if parity (PF=1)
+    Jpe(Operand),          // Jump near if parity even (PF=1)
+    Jpo(Operand),          // Jump near if parity odd (PF=0)
+    Js(Operand),           // Jump near if sign (SF=1)
+    Jz(Operand),           // Jump near if 0 (ZF=1)
+    Push(Operand),         // Push r/m64.
+    Pop(Operand),          // Pop top of stack into r64; increment stack pointer. Cannot encode 32-bit operand size.
+    SetLbl(&'static str),  // Set label
+    Call(Operand),         // Call near, absolute indirect, address given in r/m64.
+    Ret,                   // Near return to calling procedure.
 }
 //
